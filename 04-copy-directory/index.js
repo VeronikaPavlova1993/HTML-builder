cc958c1101fdaf216a6a317c5c3d500bcad3af41
Your Name <veronichka.danilina@mail.ru>
2023-05-08 15:17:41 +0400

    fead: add solution task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const filesDirnamePath = path.join(__dirname, 'files');
+const filesCopyDirnamePath = path.join(__dirname, 'files-copy');
 
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let records = await fs.readdir(src);
+
+  for (let record of records) {
+    let recordPath = path.join(src, record);
+    let destPath = path.join(dest, record);
+    let srcPathStat = await fs.stat(recordPath);
+  srcPathStat.isDirectory() ?
+      await copyDir(recordPath, destPath):
+      await fs.copyFile(recordPath, destPath);
+    }
+  }
+copyDir(filesDirnamePath, filesCopyDirnamePath);
